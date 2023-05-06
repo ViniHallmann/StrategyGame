daa6bc12e537e0c27d82c70a04d70b48303f1818
JoaoLaimer <jvlaimer@gmail.com>
2023-05-05 21:00:40 -0300

    commit

--- src/mainstrategygame/Tabuleiro.java
+++ src/mainstrategygame/Tabuleiro.java
@@ -4,20 +4,16 @@
  */
 package mainstrategygame;
 
-import java.awt.Color;
-import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import static java.lang.Math.sqrt;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.border.Border;
 import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -40,7 +36,7 @@ public class Tabuleiro extends JPanel{
     
     private Peça peçaPosicionada;
     GridBagConstraints  g = new GridBagConstraints();
-    
+       
     public Tabuleiro()
     {
         tabuleiro = new Celula[(int)(sqrt(NUMERO_DE_CASAS))][(int)(sqrt(NUMERO_DE_CASAS))];
@@ -75,7 +71,6 @@ public class Tabuleiro extends JPanel{
                     
     private void constroiTabuleiro()
     {
-        GridBagConstraints  g = new GridBagConstraints();
         g.insets = new java.awt.Insets(1, 1, 1, 1);
         for(int i = 0 ; i < sqrt(NUMERO_DE_CASAS); i++)
         {
@@ -95,13 +90,13 @@ public class Tabuleiro extends JPanel{
                 final int y = j;
                 //Ao clicar no botão pega as "informações do botão" e chama a função que coloca uma peça na posição desse botão
                 tabuleiro[i][j].addActionListener(new ActionListener() 
-                {
-                    public void actionPerformed(ActionEvent e) 
-                    {   
-                        Celula botãoClicado = (Celula) e.getSource();
-                        colocaPeçaNoTabuleiro(botãoClicado, x, y);
-                    }
-                });
+                    {
+                        public void actionPerformed(ActionEvent e) 
+                        {   
+                            Celula botãoClicado = (Celula) e.getSource();
+                            colocaPeçaNoTabuleiro(botãoClicado, x, y);
+                        }
+                    });
                 //MUDEI AS POSIÇÕES DO LAGO!!!
                 if((i == 1 && j == 2)||(i == 3 && j == 2))
                 {
@@ -115,18 +110,20 @@ public class Tabuleiro extends JPanel{
     }
     
     public void iteradorPeçasDisponiveis(char tipoDePeça){
-        if( tipoDePeça == 'B')
-            bombasDisponiveis--;
-        if( tipoDePeça == 'C')
-            caboArmeiroDisponiveis--;
-        if( tipoDePeça == 'S')
-            soldadosDisponiveis--;
-        if( tipoDePeça == 'M')
-            marechalDisponivel = false;
-        if( tipoDePeça == 'E')
-            espiaoDisponivel = false;
-        if( tipoDePeça == 'F')
-            espiaoDisponivel = false;
+       switch(tipoDePeça){
+            case 'B':
+                bombasDisponiveis--;
+            case 'C':
+                caboArmeiroDisponiveis--;
+            case 'S':
+                soldadosDisponiveis--;
+            case 'M':
+                marechalDisponivel = false;
+            case 'E':
+                espiaoDisponivel = false;
+            case 'F':
+                bandeiraDisponivel = false;
+        }
     }
     public boolean verificaPeçasDisponiveis(char tipoDePeça){
         switch(tipoDePeça){
@@ -191,15 +188,97 @@ public class Tabuleiro extends JPanel{
     }
     
     private void atualizaTabuleiro() {
-        for (int i = 0; i < sqrt(NUMERO_DE_CASAS); i++) {
-            for (int j = 0; j < sqrt(NUMERO_DE_CASAS); j++) {
-                if (peçaSelecionada != null) {
+        for (int i = 0; i < sqrt(NUMERO_DE_CASAS); i++) 
+        {
+            for (int j = 0; j < sqrt(NUMERO_DE_CASAS); j++) 
+            {
+                if (peçaSelecionada != null) 
+                {
                     tabuleiro[i][j].setEnabled(true);
-                }   else {
+                }
+                else
+                {
                     tabuleiro[i][j].setEnabled(false);
                 }
             }
         }
     }
-
+    public void setPecasComputador(){
+        Random rand = new Random();
+        int gerado = rand.nextInt(5);
+        
+        remove(tabuleiro[gerado][0]);
+        tabuleiro[gerado][0] = CelulaFactory.factory('F');
+        tabuleiro[gerado][0].setBackground(new java.awt.Color(255, 204, 204));
+        tabuleiro[gerado][0].setPreferredSize(new java.awt.Dimension(50, 50));
+                
+        g.insets = new java.awt.Insets(1, 1, 1, 1); 
+        g.gridx = gerado; g.gridy = 0;
+        add(tabuleiro[gerado][0], g);
+        atualizaTabuleiro();
+        
+        
+        boolean marechal = true;
+        boolean espiao = true;
+        int bombas = 2;
+        int soldados = 3;
+        int cabos = 2;
+        
+        for(int i = 0; i < 5; i++)
+        {            
+            for(int j = 0; j < 2; j++)
+            {
+                if((j != 0) || (i != gerado))
+                {
+                    
+                    remove(tabuleiro[i][j]);
+                    tabuleiro[i][j] = setPecaAleatoria(bombas,soldados,cabos,marechal,espiao);
+                    if(tabuleiro[i][j].getTipo() == 'B')
+                        bombas--;
+                    if(tabuleiro[i][j].getTipo() == 'S')
+                        soldados--;
+                    if(tabuleiro[i][j].getTipo() == 'C')
+                        cabos--;
+                    if(tabuleiro[i][j].getTipo() == 'M')
+                        marechal = false;
+                    if(tabuleiro[i][j].getTipo() == 'E')
+                        espiao = false;
+                    
+                    g.insets = new java.awt.Insets(1, 1, 1, 1); 
+                    g.gridx = i;
+                    g.gridy = j;
+                    add(tabuleiro[i][j], g);
+                    atualizaTabuleiro();
+                }
+            }
+        }
+    }
+    private static Celula setPecaAleatoria(int bombas, int soldados, int cabos, boolean marechal, boolean espiao )
+    {
+        
+        List<Character> tipos = new ArrayList<>();
+        if(bombas > 0)
+            tipos.add('B');
+        if(soldados > 0)
+            tipos.add('S');
+        if(cabos > 0)
+            tipos.add('C');
+        if(marechal)
+            tipos.add('M');
+        if(espiao)
+            tipos.add('E');
+        Celula celula;
+        Random random = new Random();
+        
+        char escolhido = tipos.get(random.nextInt(tipos.size()));
+        celula = CelulaFactory.factory(escolhido);
+        
+        celula.setBackground(new java.awt.Color(255, 204, 204));
+        celula.setPreferredSize(new java.awt.Dimension(50, 50));
+        //System.out.println(celula.getTipo());
+        return celula;
+        
+    }
 }
+
+    
